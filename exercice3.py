f1fac679832dbdecfaedf88003a7de8c0065048b
Tristan Lachance <tri.lachance3@outlook.com>
2021-09-16 09:09:10 -0400

    Exercices 3 avec erreur calcul

--- exercice3.py
+++ exercice3.py
@@ -2,22 +2,28 @@
 def decomposer(secondes):
 
     # TODO: Assigner à la variable "annees" le nombre d'années
-    annees =
+    annees = secondes / 31536000
 
     # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    semaines =
+    annees_restantes = annees - int(annees)
+    semaines = annees_restantes * 52
 
     # TODO: Assigner à la variable "jours" le nombre de jours restants
-    jours =
+    semaines_restantes = semaines - int(semaines)
+    jours = semaines_restantes * 7
 
     # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    heures =
+    jours_restants = jours - int(jours)
+    heures = jours_restants * 24
 
     # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    minutes =
+    heures_restantes = heures - int(heures)
+    minutes = heures_restantes * 60
 
     # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    secondes =
+    minutes_restantes = minutes - int(minutes)
+    secondes = minutes_restantes * 60
+
 
     # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
     print(annees ,semaines ,jours ,heures ,minutes ,secondes)

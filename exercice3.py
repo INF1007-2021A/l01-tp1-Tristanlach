bc7dc665ca38c53015507c90a91f9b01f9bbf2cd
Tristan Lachance <tri.lachance3@outlook.com>
2021-09-16 10:34:48 -0400

    exercice 3 fait et corrigé

--- exercice3.py
+++ exercice3.py
@@ -2,27 +2,27 @@
 def decomposer(secondes):
 
     # TODO: Assigner à la variable "annees" le nombre d'années
-    annees = secondes / 31536000
+    annees = (secondes // (3600*24*365))
+    secondes_restantes_01 = secondes - (annees * (3600*24*365))
 
     # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    annees_restantes = annees - int(annees)
-    semaines = annees_restantes * 52
+    semaines = secondes_restantes_01 // (3600*24*7)
+    secondes_restantes_02 = secondes_restantes_01 - (semaines * (3600*24*7))
 
     # TODO: Assigner à la variable "jours" le nombre de jours restants
-    semaines_restantes = semaines - int(semaines)
-    jours = semaines_restantes * 7
+    jours = secondes_restantes_02 // (3600*24)
+    secondes_restantes_03 = secondes_restantes_02 - (jours * (3600*24))
 
     # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    jours_restants = jours - int(jours)
-    heures = jours_restants * 24
+    heures = secondes_restantes_03 // (3600)
+    secondes_restantes_04 = secondes_restantes_03 - (heures * 3600)
 
-    # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    heures_restantes = heures - int(heures)
-    minutes = heures_restantes * 60
+    # TODO: Assigner à la variable "minutes" le nombre de minutes restantes
+    minutes = secondes_restantes_04 // (60)
+    secondes_restantes_05 = secondes_restantes_04 - (minutes * 60)
 
     # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    minutes_restantes = minutes - int(minutes)
-    secondes = minutes_restantes * 60
+    secondes = secondes_restantes_05
 
 
     # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
